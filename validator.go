1fdfff373979c7337b0aeb5a3551100113a3909f
Sumit Asok <sumitasok@gmail.com>
2015-02-10 15:28:29 +0530

    required is done

--- validator.go
+++ validator.go
@@ -1,9 +1,9 @@
 package validator
 
 import (
-	// "fmt"
 	"errors"
-	// "reflect"
+	// "fmt"
+	"reflect"
 )
 
 type Validator struct {
@@ -15,6 +15,34 @@ func (v *Validator) Required() *Validator {
 	if v.Object == nil {
 		v.Add("required")
 	}
+
+	switch reflect.ValueOf(v.Object).Kind() {
+	case reflect.Array, reflect.String:
+		if reflect.ValueOf(v.Object).Len() == 0 {
+			v.Add("required")
+		}
+	case reflect.Bool:
+		if !reflect.ValueOf(v.Object).Bool() {
+			v.Add("required")
+		}
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		if reflect.ValueOf(v.Object).Int() == 0 {
+			v.Add("required")
+		}
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		if reflect.ValueOf(v.Object).Uint() == 0 {
+			v.Add("required")
+		}
+	case reflect.Float32, reflect.Float64:
+		if reflect.ValueOf(v.Object).Float() == 0 {
+			v.Add("required")
+		}
+	case reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice:
+		if reflect.ValueOf(v.Object).IsNil() {
+			v.Add("required")
+		}
+	}
+
 	// obj := dereference(objPtr)
 	// if timeVal, ok := obj.(time.Time); ok {
 	// 	return !timeVal.IsZero()
--- validator_test.go
+++ validator_test.go
@@ -6,12 +6,17 @@ import (
 	"testing"
 )
 
+type TestStruct struct {
+	Name string
+}
+
 func TestValidator(t *testing.T) {
 	assert := assert.New(t)
 
 	v := On("Sumit").Required()
 	assert.NoError(v.Error())
 
-	assert.True(true)
-
+	ts := TestStruct{}
+	v1 := On(ts.Name).Required()
+	assert.Error(v1.Error())
 }
