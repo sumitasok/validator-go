d94ac9837dbc6b7abede848e39b62a58243c7520
Sumit Asok <sumitasok@gmail.com>
2015-02-11 09:36:25 +0530

    custom message support

--- validator.go
+++ validator.go
@@ -19,31 +19,31 @@ type Validator struct {
 	Errors  []error
 }
 
-func (v *Validator) Range(min int, max int) *Validator {
-	v.Min(min)
-	v.Max(max)
+func (v *Validator) Range(min int, max int, msg ...string) *Validator {
+	v.Min(min, msg...)
+	v.Max(max, msg...)
 	return v
 }
 
-func (v *Validator) IsTimeBefore(t time.Time) *Validator {
+func (v *Validator) IsTimeBefore(t time.Time, msg ...string) *Validator {
 	if dTime, ok := v.Object.(time.Time); ok {
 		if dTime.After(t) {
-			v.Add("time should be before " + t.String())
+			v.Add("time should be before "+t.String(), msg...)
 		}
 	} else {
-		v.Add("cannot be applied on this object")
+		v.Add("cannot be applied on this object", msg...)
 	}
 
 	return v
 }
 
-func (v *Validator) IsTimeAfter(t time.Time) *Validator {
+func (v *Validator) IsTimeAfter(t time.Time, msg ...string) *Validator {
 	if dTime, ok := v.Object.(time.Time); ok {
 		if dTime.Before(t) {
-			v.Add("time should be after " + t.String())
+			v.Add("time should be after "+t.String(), msg...)
 		}
 	} else {
-		v.Add("cannot be applied on this object")
+		v.Add("cannot be applied on this object", msg...)
 	}
 
 	return v
@@ -53,11 +53,7 @@ func (v *Validator) Match(pattern string, msg ...string) *Validator {
 	if str, ok := v.Object.(string); ok {
 		matched, err := regexp.MatchString(pattern, str)
 		if err != nil || matched == false {
-			if len(msg) == 0 {
-				v.Add("pattern mismatch")
-			} else {
-				v.Add(msg[0])
-			}
+			v.Add("pattern mismatch", msg...)
 		} else {
 			v.Add("matching cannot be applied on this object")
 		}
@@ -75,36 +71,36 @@ func (v *Validator) Email(msg ...string) *Validator {
 	return v.Match(pattern, msg...)
 }
 
-func (v *Validator) Max(maxI interface{}) *Validator {
+func (v *Validator) Max(maxI interface{}, msg ...string) *Validator {
 	switch reflect.ValueOf(v.Object).Kind() {
 	case reflect.Array, reflect.String:
 		if max, ok := maxI.(int); ok {
 			if reflect.ValueOf(v.Object).Len() > max {
-				v.Add("maximum " + strconv.Itoa(max) + " characters " + "allowed")
+				v.Add("maximum "+strconv.Itoa(max)+" characters "+"allowed", msg...)
 			}
 		}
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		if max, ok := maxI.(int); ok {
 			if reflect.ValueOf(v.Object).Int() > int64(max) {
-				v.Add("maximum " + strconv.Itoa(max) + " is allowed")
+				v.Add("maximum "+strconv.Itoa(max)+" is allowed", msg...)
 			}
 		}
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
 		if max, ok := maxI.(int); ok {
 			if reflect.ValueOf(v.Object).Uint() > uint64(max) {
-				v.Add("maximum" + strconv.Itoa(max) + "is allowed")
+				v.Add("maximum"+strconv.Itoa(max)+"is allowed", msg...)
 			}
 		}
 	case reflect.Float32, reflect.Float64:
 		if max, ok := maxI.(int); ok {
 			if reflect.ValueOf(v.Object).Float() > float64(max) {
-				v.Add("maximum " + strconv.Itoa(max) + " is allowed")
+				v.Add("maximum "+strconv.Itoa(max)+" is allowed", msg...)
 			}
 		}
 	case reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice:
 		if max, ok := maxI.(int); ok {
 			if reflect.ValueOf(v.Object).Len() > max {
-				v.Add("maximum " + strconv.Itoa(max) + " numbers allowed")
+				v.Add("maximum "+strconv.Itoa(max)+" numbers allowed", msg...)
 			}
 		}
 	case reflect.Struct:
@@ -120,36 +116,36 @@ func (v *Validator) Max(maxI interface{}) *Validator {
 	return v
 }
 
-func (v *Validator) Min(minI interface{}) *Validator {
+func (v *Validator) Min(minI interface{}, msg ...string) *Validator {
 	switch reflect.ValueOf(v.Object).Kind() {
 	case reflect.Array, reflect.String:
 		if min, ok := minI.(int); ok {
 			if reflect.ValueOf(v.Object).Len() < min {
-				v.Add("minimum " + strconv.Itoa(min) + " characters " + "required")
+				v.Add("minimum "+strconv.Itoa(min)+" characters "+"required", msg...)
 			}
 		}
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		if min, ok := minI.(int64); ok {
 			if reflect.ValueOf(v.Object).Int() < min {
-				v.Add("minimum " + strconv.Itoa(int(min)) + " is required")
+				v.Add("minimum "+strconv.Itoa(int(min))+" is required", msg...)
 			}
 		}
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
 		if min, ok := minI.(uint64); ok {
 			if reflect.ValueOf(v.Object).Uint() < min {
-				v.Add("minimum" + strconv.Itoa(int(min)) + "is required")
+				v.Add("minimum"+strconv.Itoa(int(min))+"is required", msg...)
 			}
 		}
 	case reflect.Float32, reflect.Float64:
 		if min, ok := minI.(float64); ok {
 			if reflect.ValueOf(v.Object).Float() < min {
-				v.Add("minimum " + strconv.Itoa(int(min)) + " is required")
+				v.Add("minimum "+strconv.Itoa(int(min))+" is required", msg...)
 			}
 		}
 	case reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice:
 		if min, ok := minI.(int); ok {
 			if reflect.ValueOf(v.Object).Len() < min {
-				v.Add("minimum " + strconv.Itoa(min) + " numbers required")
+				v.Add("minimum "+strconv.Itoa(min)+" numbers required", msg...)
 			}
 		}
 	case reflect.Struct:
@@ -165,36 +161,36 @@ func (v *Validator) Min(minI interface{}) *Validator {
 	return v
 }
 
-func (v *Validator) Required() *Validator {
+func (v *Validator) Required(msg ...string) *Validator {
 	if v.Object == nil {
-		v.Add("required")
+		v.Add("required", msg...)
 	}
 
 	// inspired by https://github.com/jamieomatthews/validation
 	switch reflect.ValueOf(v.Object).Kind() {
 	case reflect.Array, reflect.String:
 		if reflect.ValueOf(v.Object).Len() == 0 {
-			v.Add("required")
+			v.Add("required", msg...)
 		}
 	case reflect.Bool:
 		if !reflect.ValueOf(v.Object).Bool() {
-			v.Add("required")
+			v.Add("required", msg...)
 		}
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		if reflect.ValueOf(v.Object).Int() == 0 {
-			v.Add("required")
+			v.Add("required", msg...)
 		}
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
 		if reflect.ValueOf(v.Object).Uint() == 0 {
-			v.Add("required")
+			v.Add("required", msg...)
 		}
 	case reflect.Float32, reflect.Float64:
 		if reflect.ValueOf(v.Object).Float() == 0 {
-			v.Add("required")
+			v.Add("required", msg...)
 		}
 	case reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice:
 		if reflect.ValueOf(v.Object).IsNil() {
-			v.Add("required")
+			v.Add("required", msg...)
 		}
 	case reflect.Struct:
 		if _, ok := v.Object.(time.Time); ok {
@@ -220,7 +216,11 @@ func (v Validator) Error() error {
 	return v.Errors[0]
 }
 
-func (v *Validator) Add(msg string) {
+func (v *Validator) Add(msg string, customMsg ...string) {
+	if len(customMsg) != 0 {
+		msg = customMsg[0]
+	}
+
 	if v.KeyName == "" {
 		v.Errors = append(v.Errors, errors.New(msg))
 	} else {
