2aca944b9ab9f88193d37e221f2b7214cf83e496
Sumit Asok <sumitasok@gmail.com>
2015-02-12 20:31:36 +0530

    better error check and messages

--- max.go
+++ max.go
@@ -3,7 +3,6 @@ package validator
 import (
 	"fmt"
 	"reflect"
-	"strconv"
 	"time"
 )
 
@@ -14,7 +13,7 @@ type Max struct {
 func (m *Max) Allow(obj interface{}) bool {
 	objType := reflect.ValueOf(obj).Kind()
 	valueType := reflect.ValueOf(m.V.Object).Kind()
-	fmt.Println(objType)
+	fmt.Println(objType, valueType)
 	switch objType {
 	case reflect.Int:
 		switch valueType {
@@ -25,37 +24,54 @@ func (m *Max) Allow(obj interface{}) bool {
 	return false
 }
 
+func typeAllow(a interface{}, b interface{}) bool {
+	if reflect.ValueOf(a).Kind() == reflect.Struct && reflect.ValueOf(b).Kind() != reflect.Struct {
+		return false
+	}
+
+	if reflect.ValueOf(a).Kind() != reflect.Struct && reflect.ValueOf(b).Kind() == reflect.Struct {
+		return false
+	}
+
+	return true
+}
+
 func (m *Max) validate(v *Validator, maxI interface{}, msg ...string) {
+	if !typeAllow(maxI, v.Object) {
+		v.Add("cannot be applied on this object")
+		return
+	}
+
 	switch reflect.ValueOf(v.Object).Kind() {
 	case reflect.Array, reflect.String:
-		if max, ok := maxI.(int); ok {
-			if reflect.ValueOf(v.Object).Len() > max {
-				v.Add("maximum "+strconv.Itoa(max)+" characters "+"allowed", msg...)
-			}
+		max := reflect.ValueOf(maxI).Int()
+		if int64(reflect.ValueOf(v.Object).Len()) > max {
+			defaultMsg := fmt.Sprintf("maximum %x characters allowed", max)
+			v.Add(defaultMsg, msg...)
 		}
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		if max, ok := maxI.(int); ok {
-			if reflect.ValueOf(v.Object).Int() > int64(max) {
-				v.Add("maximum "+strconv.Itoa(max)+" is allowed", msg...)
-			}
+		max := reflect.ValueOf(maxI).Int()
+		if reflect.ValueOf(v.Object).Int() > max {
+			defaultMsg := fmt.Sprintf("maximum %x is allowed", max)
+			v.Add(defaultMsg, msg...)
 		}
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		if max, ok := maxI.(int); ok {
-			if reflect.ValueOf(v.Object).Uint() > uint64(max) {
-				v.Add("maximum"+strconv.Itoa(max)+"is allowed", msg...)
-			}
+		max := reflect.ValueOf(maxI).Uint()
+		if reflect.ValueOf(v.Object).Uint() > max {
+			defaultMsg := fmt.Sprintf("maximum %x is allowed", max)
+			v.Add(defaultMsg, msg...)
 		}
 	case reflect.Float32, reflect.Float64:
-		if max, ok := maxI.(int); ok {
-			if reflect.ValueOf(v.Object).Float() > float64(max) {
-				v.Add("maximum "+strconv.Itoa(max)+" is allowed", msg...)
-			}
+		max := reflect.ValueOf(maxI).Float()
+		if reflect.ValueOf(v.Object).Float() > max {
+			defaultMsg := fmt.Sprintf("maximum %x is allowed", max)
+			v.Add(defaultMsg, msg...)
 		}
 	case reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice:
-		if max, ok := maxI.(int); ok {
-			if reflect.ValueOf(v.Object).Len() > max {
-				v.Add("maximum "+strconv.Itoa(max)+" numbers allowed", msg...)
-			}
+		max := reflect.ValueOf(maxI).Int()
+		if int64(reflect.ValueOf(v.Object).Len()) > max {
+			defaultMsg := fmt.Sprintf("maximum %x numbers allowed", max)
+			v.Add(defaultMsg, msg...)
 		}
 	case reflect.Struct:
 		if t, ok := maxI.(time.Time); ok {
