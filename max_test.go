424af472cdc256e847178974422d7d597c324e6e
Sumit Asok <sumitasok@gmail.com>
2015-02-13 19:03:11 +0530

    removing not used code

--- max_test.go
+++ max_test.go
@@ -10,36 +10,6 @@ var (
 	wont = "cannot be applied on this object"
 )
 
-// 	Int8Value := int8(17) // -128 to 127, Signed 8-bit integer, bytes per element 1
-// 	// Int16Value := int16(7) -32,768 to 32,767, Signed 16-bit integer, bytes per element 2
-// 	// Int32Value := int32(7) -2,147,483,648 to 2,147,483,647, Signed 32-bit integer, bytes per element 4
-// 	// Int64Value := int64(7) -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807, Signed 64-bit intege, bytes per element 8
-// 	// UintValue := uint(7)
-// 	// Uint8Value := uint8(7)
-// 	// Uint16Value := uint16(7)
-// 	// Uint32Value := uint32(7)
-// 	// Uint64Value := uint64(7)
-// 	// UintptrValue := uintptr(7)
-// 	// Float32Value := float32(7)
-// 	// Float64Value := float64(7)
-// 	// InterfaceValue := 7
-// 	// MapValue := map[int]string{7}
-// 	// PtrValue := 7
-// 	// SliceValue := 7
-// 	v := false
-// 	k1, ok1 := makeInterface(IntValue).(int)
-// 	k1tc := int64(IntValue)
-// 	fmt.Println("k1", k1, "ok1", ok1, "k1tc", k1tc)
-// 	k2, ok2 := makeInterface(Int8Value).(int8)
-// 	k2tc := float64(Int8Value)
-// 	// k264, ok2 := makeInterface(k2tc).(float64)
-// 	fmt.Println("k2", k2, "ok2", ok2, "k2tc", k2tc, float64(1.1))
-// 	if k2tc > float64(makeInterface(Int8Value)) {
-// 		v = true
-// 	}
-// 	assert.True(v)
-// }
-
 type testStr struct{}
 
 func TestTypeAllow(t *testing.T) {
