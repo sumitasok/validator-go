177fc273817d3000c89644b9f34e29a83449be92
Sumit Asok <sumitasok@gmail.com>
2015-02-13 16:41:49 +0530

    trailing code removed

--- max.go
+++ max.go
@@ -11,20 +11,6 @@ type Max struct {
 	V *Validator
 }
 
-func (m *Max) Allow(obj interface{}) bool {
-	objType := reflect.ValueOf(obj).Kind()
-	valueType := reflect.ValueOf(m.V.Object).Kind()
-	fmt.Println(objType, valueType)
-	switch objType {
-	case reflect.Int:
-		switch valueType {
-		case reflect.Array, reflect.String, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr, reflect.Float32, reflect.Float64, reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice:
-			return true
-		}
-	}
-	return false
-}
-
 func typeAllow(a interface{}, b interface{}) bool {
 	if reflect.ValueOf(a).Kind() == reflect.Struct && reflect.ValueOf(b).Kind() != reflect.Struct {
 		return false
